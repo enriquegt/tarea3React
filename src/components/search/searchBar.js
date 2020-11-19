78fb12810c2bd18f9489b9638ce3f54ea5930ae2
JuanCarlosLP <jclarios.js@gmail.com>
2020-11-18 19:00:51 -0600

    Separando SearchBar

--- src/components/search/searchBar.js
+++ src/components/search/searchBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Icon from '../icon/icon';
+import {users} from '../users/usersDB';
+import SearchDropdown from '../users/users'
 import {
     SearchSolid
 } from "@graywolfai/react-heroicons";
@@ -8,7 +10,17 @@ import './styles.css'
 class searchBar extends React.Component {
     constructor() {
         super();
+        this.state = {
+            searchUser: "",
+            users: users
+          };
     }
+
+    handleSearch = (evento) => {
+        //Capturar lo que está escribiendo el usuario
+        const user = evento.target.value;
+        this.setState({ searchUser: user });
+      };
 
     render() {
         
@@ -16,8 +28,28 @@ class searchBar extends React.Component {
             <div className="container-search">
                 <div className="icoBuscar"> <Icon source={< SearchSolid />}/> </div>
                 <form className="form-search">
-                <input placeholder=  "Buscar en Tweeter" />
+                <input placeholder=  "Buscar en Twitter" 
+                    type="text"
+                    className="form-control"
+                    id="formGroupExampleInput"
+                    placeholder="Buscar en Tweeter"
+                    onChange={this.handleSearch}
+                    value={this.state.searchUser}
+                />
                     <div className="buscar">
+                    {this.state.users
+                  .filter((user) =>
+                    user.profile
+                      .toLowerCase()
+                      .includes(this.state.searchUser.toLowerCase())
+                  )
+                  .map((user) => {
+                        <SearchDropdown 
+                            profileImg= {user.profileImg}
+                            profile= {user.profile}
+                            username= {user.username}
+                        />
+                  })}
                     </div>
                 </form>
 
--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,71 +1,31 @@
 import React from "react";
-import {users} from './usersDB';
 import './styles.css';
 
-class TwitterUsers extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        searchUser: "",
-        users: users
-      };
-    }
-  
-    handleSearch = (evento) => {
-      //Capturar lo que está escribiendo el usuario
-      const user = evento.target.value;
-      this.setState({ searchUser: user });
-    };
-  
+class SearchDropdown extends React.Component {
+  constructor() {
+    super();
+}
+
+
     render() {
-      return (
-        <div className="container">
-          <div className="row">
-            
-            <div className="col-md-7">
-            <form>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="formGroupExampleInput"
-                  placeholder="Buscar en Tweeter"
-                  onChange={this.handleSearch}
-                  value={this.state.searchUser}
-                />
-              </div>
-            </form>
-              <div className="list-group">
-                {this.state.users
-                  .filter((user) =>
-                    user.profile
-                      .toLowerCase()
-                      .includes(this.state.searchUser.toLowerCase())
-                  )
-                  .map((user) => {
-                    return (
-                        <div className="tweet-container">
-                        <div className="row">
-                    
-                                   <div className="c1">
-                                           <img src={user.profileImg} alt="profile" />
-                                       </div>
-                    
-                                      <div className="c2">
-                                      <h6>{user.profile}</h6>
-                                            <h6>{user.username}</h6>
-                                  </div>
-                    
-                        </div>
+        return (
+            <div className="tweet-container">
+                <div className="row">
+
+                    <div className="c1">
+                    {console.log(this.props.profile)}
+                        <img src={this.props.profileImg} alt="profile"/>
                     </div>
-                    );
-                  })}
-              </div>
+                    
+                    <div className="c2">
+                        <h6>{this.props.profile}</h6>
+                        <h6>{this.props.username}</h6>
+                    </div>
+                </div>
+                  
             </div>
-          </div>
-        </div>
-      );
+        )
     }
-  }
-  
-  export default TwitterUsers;
+ 
+}
+  export default SearchDropdown;
